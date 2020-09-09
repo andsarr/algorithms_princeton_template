fa9d4937aa167367711900de70e5b5442d537367
Gianluca Gippetto <gianluca.gippetto@gmail.com>
2020-09-10 01:26:22 +0200

    Add template for "Seam Carving" assignment (2nd of part II)

---
+++ src/main/java/edu/princeton/cs/algorithms/seam/SeamCarver.java
@@ -0,0 +1,69 @@
+package edu.princeton.cs.algorithms.seam;
+
+import edu.princeton.cs.algs4.Picture;
+
+public class SeamCarver {
+
+    public SeamCarver(Picture picture) {
+
+    }
+
+    /**
+     * Returns the current picture.
+      */
+    public Picture picture() {
+        return null;
+    }
+
+    /**
+     * Returns the width of current picture.
+     */
+    public int width() {
+        return 0;
+    }
+
+    /**
+     * Returns the height of current picture.
+     */
+    public int height() {
+        return 0;
+    }
+
+    /**
+     * Returns the energy of pixel at column x and row y.
+     */
+    public double energy(int x, int y) {
+        return 0;
+    }
+
+    /**
+     * Returns the sequence of indices for horizontal seam.
+     */
+    public int[] findHorizontalSeam() {
+        return null;
+    }
+
+    /**
+     * Returns the sequence of indices for vertical seam.
+     */
+    public int[] findVerticalSeam() {
+        return null;
+    }
+
+    /**
+     * Removes horizontal seam from current picture.
+     */
+    public void removeHorizontalSeam(int[] seam) {
+    }
+
+    /**
+     * Removes vertical seam from current picture
+     */
+    public void removeVerticalSeam(int[] seam) {
+    }
+
+    //  unit testing (optional)
+    public static void main(String[] args) {
+    }
+
+}
